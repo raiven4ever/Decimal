816b861ac9da493e8553c5f5b382f2cfcb3f9df3
craponyourlap <143549907+craponyourlap@users.noreply.github.com>
2025-09-29 20:10:41 -0400

    Update Cache.java

--- src/decimal/helpers/Cache.java
+++ src/decimal/helpers/Cache.java
@@ -1,27 +1,39 @@
 package decimal.helpers;
 
-import java.util.Arrays;
-
 import decimal.Decimal;
 
+/**
+ * Simple fixed-size cache for storing recent {@link Decimal} values.
+ *
+ * <p>This cache is primarily used to detect repeated values during
+ * iterative algorithms (such as Newton–Raphson) to prevent infinite loops.</p>
+ *
+ * <p>The cache maintains the most recent values in a FIFO-like manner:
+ * <ul>
+ *   <li>New values are inserted at the front.</li>
+ *   <li>Older values are shifted back.</li>
+ *   <li>Equality is determined using {@code Decimal.equals()}.</li>
+ * </ul>
+ * </p>
+ */
 public class Cache {
-	
+
 	private Decimal[] cache;
-	
+
 	public Cache(int size, Decimal initialValue) {
 		cache = new Decimal[size];
 		for (int i = 0; i < size; i++) {
 			cache[i] = initialValue;
 		}
 	}
-	
+
 	public boolean contains(Decimal value) {
 		for (Decimal x : cache) {
 			if (x.equals(value)) return true;
 		}
 		return false;
 	}
-	
+
 	public void update(Decimal value) {
 		for (int i = cache.length - 1; i > 0; i--)
 			cache[i] = cache[i-1];
